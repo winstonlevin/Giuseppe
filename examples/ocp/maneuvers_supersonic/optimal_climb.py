1a9a9a3bf7ca9eba862ee8b42c7420686c2e0d2c
winstonlevin <wlevin@purdue.edu>
2023-05-08 13:21:02 -0400

    Continued work on optimal climb example

--- examples/ocp/maneuvers_supersonic/optimal_climb.py
+++ examples/ocp/maneuvers_supersonic/optimal_climb.py
@@ -14,15 +14,15 @@ ocp.set_independent(t)
 
 # Controls
 alpha = ca.MX.sym('alpha', 1)
-sigma = ca.MX.sym('sigma', 1)
+# sigma = ca.MX.sym('sigma', 1)
 ocp.add_control(alpha)
-ocp.add_control(sigma)
+# ocp.add_control(sigma)
 
 # States
 h = ca.MX.sym('h', 1)
 v = ca.MX.sym('v', 1)
 gam = ca.MX.sym('gam', 1)
-psi = ca.MX.sym('psi', 1)
+# psi = ca.MX.sym('psi', 1)
 m = ca.MX.sym('m', 1)
 
 # Immutable Constant Parameters
@@ -47,7 +47,7 @@ CLalpha = cl_alpha_table(M)
 cd0 = cd0_table(M)
 
 # Aerodynamics
-qdyn = 0.5 * rho * v**2
+qdyn = 0.5 * rho * v ** 2
 cl = CLalpha * alpha
 cd = cd0 + eta * CLalpha * alpha ** 2
 lift = qdyn * s_ref * cl
@@ -55,14 +55,9 @@ drag = qdyn * s_ref * cd
 
 # Dynamics
 ocp.add_state(h, v * ca.sin(gam))
-ocp.add_state(v, (thrust * ca.cos(alpha) - drag) / m - g * ca.cos(gam))
-ocp.add_state(gam, (thrust * ca.sin(alpha) + lift) / (m * v) - g/v * ca.cos(gam))
+ocp.add_state(v, (thrust * ca.cos(alpha) - drag) / m - g * ca.sin(gam))
+ocp.add_state(gam, (thrust * ca.sin(alpha) + lift) / (m * v) - g / v * ca.cos(gam))
 ocp.add_state(m, -thrust / (Isp * g0))
-
-# Cost
-weight_time = ca.MX.sym('Wt')  # 1.0 -> min time, 0.0 -> min fuel
-ocp.add_constant(weight_time, 1.0)
-ocp.set_cost(0, 0, weight_time * t + (1 - weight_time) * m)
 
 # Boundary Conditions
 h0 = ca.MX.sym('h0')
@@ -73,14 +68,79 @@ m0 = ca.MX.sym('m0')
 ocp.add_constant(h0, 0.)
 ocp.add_constant(v0, 0.38 * atm.speed_of_sound(0.))
 ocp.add_constant(gam0, 0.)
-ocp.add_constant(m0, 34_200.)
+# ocp.add_constant(m0, 34_200. / g0)
+ocp.add_constant(m0, 20_000. / g0)
+
+t_ref = 100.
+v_ref = (3 * atm.speed_of_sound(65_600.) - 0.38 * atm.speed_of_sound(0.)) / 2
+gam_ref = 30 * np.pi / 180
+h_ref = 65_600. / 2
+m_ref = 34_200. / 2
+
+ocp.add_constraint(location='initial', expr=t / t_ref)
+ocp.add_constraint(location='initial', expr=(h - h0) / h_ref)
+ocp.add_constraint(location='initial', expr=(v - v0) / v_ref)
+ocp.add_constraint(location='initial', expr=(gam - gam0) / gam_ref)
+ocp.add_constraint(location='initial', expr=(m - m0) / h_ref)
 
 hf = ca.MX.sym('hf')
 vf = ca.MX.sym('vf')
 gamf = ca.MX.sym('gamf')
 
 ocp.add_constant(hf, 65_600.)
-ocp.add_constant(vf, 3 * atm.speed_of_sound(0.))
+ocp.add_constant(vf, 3 * atm.speed_of_sound(65_600.))
 ocp.add_constant(gamf, 0.)
 
-# TODO -- guess generation, continuations
+ocp.add_constraint(location='terminal', expr=(h - hf) / h_ref)
+ocp.add_constraint(location='terminal', expr=(v - vf) / v_ref)
+ocp.add_constraint(location='terminal', expr=(gam - gamf) / gam_ref)
+
+# Cost
+weight_time = ca.MX.sym('Wt')  # 1.0 -> min time, 0.0 -> min fuel
+ocp.add_constant(weight_time, 1.0)
+ocp.set_cost(0, 0, weight_time * t / t_ref + (1 - weight_time) * m / m_ref)
+
+# Compilation
+with giuseppe.utils.Timer(prefix='Compilation Time:'):
+    adiff_dual = giuseppe.problems.automatic_differentiation.ADiffDual(ocp)
+    num_solver = giuseppe.numeric_solvers.SciPySolver(adiff_dual, verbose=2, max_nodes=100, node_buffer=10)
+
+
+# # Proportional Ctrl Law to Drive gam -> gam_cmd
+# def ctrl_law(_t, _x, _p, _k):
+#     _gain = -np.log(0.02) / 2  # 2 second settling time
+#     _gam_cmd = 0.  # FPA command
+#     _alpha_max = 10. * np.pi / 180
+#
+#     _h = _x[0]
+#     _v = _x[1]
+#     _gam = _x[2]
+#     _m = _x[3]
+#
+#     _mach = _v / atm.speed_of_sound(_h)
+#     _lift_alpha = 0.5 * atm.density(_h) * _v ** 2 * cl_alpha_table(_mach)
+#     _thrust = thrust_table((_mach, _h))
+#     _alpha = float((_gain * _m * _v * (_gam_cmd - _gam) + _m * (mu / (Re + _h) ** 2) * np.cos(_gam)) \
+#                    / (_thrust + _lift_alpha))
+#     _alpha = min(max(_alpha, -_alpha_max), _alpha_max)
+#     return np.array((_alpha,))
+
+
+guess = giuseppe.guess_generation.auto_propagate_guess(adiff_dual, control=9 * np.pi/180, t_span=5)
+
+with open('guess.data', 'wb') as file:
+    pickle.dump(guess, file)
+
+seed_sol = num_solver.solve(guess)
+
+with open('seed_sol.data', 'wb') as file:
+    pickle.dump(seed_sol, file)
+
+# Continuations (from guess BCs to desired BCs)
+cont = giuseppe.continuation.ContinuationHandler(num_solver, seed_sol)
+cont.add_linear_series(100, {'vf': 0.5 * atm.speed_of_sound(0.), 'hf': 0., 'gamf': 0.})
+cont.add_linear_series(100, {'hf': 65_600., 'vf': 3. * atm.speed_of_sound(65_600.)})
+sol_set = cont.run_continuation()
+
+# Save Solution
+sol_set.save('sol_set.data')
---
+++ examples/ocp/maneuvers_supersonic/plot_optimal_climb.py
@@ -0,0 +1,107 @@
+import pickle
+import matplotlib.pyplot as plt
+import numpy as np
+import matplotlib as mpl
+
+from lookup_tables import cl_alpha_table, cd0_table, thrust_table, atm
+
+mpl.rcParams['axes.formatter.useoffset'] = False
+
+DATA = 2
+
+if DATA == 0:
+    with open('guess.data', 'rb') as f:
+        sol = pickle.load(f)
+elif DATA == 1:
+    with open('seed_sol.data', 'rb') as f:
+        sol = pickle.load(f)
+else:
+    with open('sol_set.data', 'rb') as f:
+        sols = pickle.load(f)
+        sol = sols[-1]
+
+
+# Constants
+r2d = 180 / np.pi
+g0 = 32.174
+s_ref = 500.
+eta = 1.0
+
+# PLOT STATES
+ylabs = (r'$h$ [ft]', r'$V$ [ft/s]', r'$\gamma$ [deg]', r'$m$ [lbm]')
+ymult = np.array((1., 1., r2d, g0))
+fig_states = plt.figure()
+axes_states = []
+
+for idx, state in enumerate(list(sol.x)):
+    axes_states.append(fig_states.add_subplot(2, 2, idx + 1))
+    ax = axes_states[-1]
+    ax.grid()
+    ax.set_xlabel('Time [s]')
+    ax.set_ylabel(ylabs[idx])
+    ax.plot(sol.t, state * ymult[idx])
+
+fig_states.suptitle(f'Cost(Wt = {sol.k[-1]}) = {sol.cost}, tf = {sol.t[-1]}, mf = {sol.x[-1, -1]}')
+fig_states.tight_layout()
+
+
+# PLOT U
+ylabs = (r'$\alpha$ [deg]',)
+ymult = np.array((r2d,))
+fig_u = plt.figure()
+axes_u = []
+
+for idx, ctrl in enumerate(list(sol.u)):
+    axes_u.append(fig_u.add_subplot(1, 1, idx + 1))
+    ax = axes_u[-1]
+    ax.grid()
+    ax.set_xlabel('Time [s]')
+    ax.set_ylabel(ylabs[idx])
+    ax.plot(sol.t, ctrl * ymult[idx])
+
+fig_u.tight_layout()
+
+# PLOT COSTATES
+ylabs = (r'$\lambda_{h}$', r'$\lambda_{V}$', r'$\lambda_{\gamma}$', r'$\lambda_{m}$')
+ymult = np.array((1., 1., r2d, g0))
+fig_costates = plt.figure()
+axes_costates = []
+
+for idx, costate in enumerate(list(sol.lam)):
+    axes_costates.append(fig_costates.add_subplot(2, 2, idx + 1))
+    ax = axes_costates[-1]
+    ax.grid()
+    ax.set_xlabel('Time [s]')
+    ax.set_ylabel(ylabs[idx])
+    ax.plot(sol.t, costate * ymult[idx])
+
+fig_costates.suptitle(f'Cost(Wt = {sol.k[-1]}) = {sol.cost}, tf = {sol.t[-1]}, mf = {sol.x[-1, -1]}')
+fig_costates.tight_layout()
+
+# PLOT FORCES
+lift = np.empty(sol.t.shape)
+drag = np.empty(sol.t.shape)
+thrust = np.empty(sol.t.shape)
+
+for idx, (x, u) in enumerate(zip(list(sol.x.T), list(sol.u.T))):
+    _mach = x[1] / atm.speed_of_sound(x[0])
+    _qdyn = 0.5 * x[1] ** 2 * atm.density(x[0])
+    lift[idx] = float(_qdyn * s_ref * cl_alpha_table(_mach) * u[0])
+    drag[idx] = float(_qdyn * s_ref * (cd0_table(_mach) + eta * cl_alpha_table(_mach) * u[0] ** 2))
+    thrust[idx] = float(thrust_table((_mach, x[0])))
+
+ylabs = (r'$L$ [g]', r'$D$ [g]', r'$T$ [g]')
+ydata = (lift, drag, thrust)
+
+fig_aero = plt.figure()
+
+for idx, y in enumerate(ydata):
+    ax = fig_aero.add_subplot(3, 1, idx + 1)
+    ax.plot(sol.t, y / (sol.x[3] * g0))
+    ax.grid()
+    ax.set_xlabel('Time [s]')
+    ax.set_ylabel(ylabs[idx])
+
+fig_aero.tight_layout()
+
+plt.show()
