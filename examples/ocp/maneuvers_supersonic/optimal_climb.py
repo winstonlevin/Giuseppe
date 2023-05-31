Correct costate scaling for climb problem

examples/ocp/maneuvers_supersonic/optimal_climb.py
@@ -34,6 +34,7 @@ mu = 1.4076539e16
 Re = 20902900
 g0 = mu / Re ** 2
 g = mu / (Re + h) ** 2
+# g = g0
 
 # Look-Up Tables & Atmospheric Expressions
 T = temp_table(h)
@@ -59,6 +60,25 @@ ocp.add_state(v, (thrust * ca.cos(alpha) - drag) / m - g * ca.sin(gam))
 ocp.add_state(gam, (thrust * ca.sin(alpha) + lift) / (m * v) - g / v * ca.cos(gam))
 ocp.add_state(m, -thrust / (Isp * g0))
 
+# Reference Scales
+t_ref = ca.MX.sym('t_ref')
+v_ref = ca.MX.sym('v_ref')
+gam_ref = ca.MX.sym('gam_ref')
+h_ref = ca.MX.sym('h_ref')
+m_ref = ca.MX.sym('m_ref')
+
+t_ref_val = 100.
+v_ref_val = (3 * atm.speed_of_sound(65_600.) - 0.38 * atm.speed_of_sound(0.)) / 2
+gam_ref_val = 30 * np.pi / 180
+h_ref_val = 65_600. / 2
+m_ref_val = (34_200. / g0) / 2
+
+ocp.add_constant(t_ref, t_ref_val)
+ocp.add_constant(v_ref, v_ref_val)
+ocp.add_constant(gam_ref, gam_ref_val)
+ocp.add_constant(h_ref, h_ref_val)
+ocp.add_constant(m_ref, m_ref_val)
+
 # Boundary Conditions
 h0 = ca.MX.sym('h0')
 v0 = ca.MX.sym('v0')
@@ -70,12 +90,6 @@ ocp.add_constant(v0, 0.38 * atm.speed_of_sound(0.))
 ocp.add_constant(gam0, 0.)
 # ocp.add_constant(m0, 34_200. / g0)
 ocp.add_constant(m0, 20_000. / g0)
-
-t_ref = 100.
-v_ref = (3 * atm.speed_of_sound(65_600.) - 0.38 * atm.speed_of_sound(0.)) / 2
-gam_ref = 30 * np.pi / 180
-h_ref = 65_600. / 2
-m_ref = 34_200. / g0 / 2
 
 ocp.add_constraint(location='initial', expr=t / t_ref)
 ocp.add_constraint(location='initial', expr=(h - h0) / h_ref)
@@ -100,13 +114,44 @@ weight_time = ca.MX.sym('Wt')  # 1.0 -> min time, 0.0 -> min fuel
 ocp.add_constant(weight_time, 1.0)
 ocp.set_cost(0, 0, weight_time * t / t_ref + (1 - weight_time) * m / m_ref)
 
+# # Path Constraint (h > 0)
+# h_min = ca.MX.sym('h_min')
+# h_max = ca.MX.sym('h_max')
+# eps_h = ca.MX.sym('eps_h')
+#
+# ocp.add_constant(h_min, -2.e3)
+# ocp.add_constant(h_max, 105e3)
+# ocp.add_constant(eps_h, 1e-3)
+#
+# ocp.add_inequality_constraint(
+#         'path', h,
+#         lower_limit=h_min, upper_limit=h_max,
+#         regularizer=giuseppe.problems.automatic_differentiation.regularization.ADiffPenaltyConstraintHandler(
+#                 regulator=eps_h / h_max))
+#
+# mach_min = ca.MX.sym('mach_min')
+# mach_max = ca.MX.sym('mach_max')
+# eps_mach = ca.MX.sym('eps_mach')
+#
+# ocp.add_constant(mach_min, 0.)
+# ocp.add_constant(mach_max, 3.2)
+# ocp.add_constant(eps_mach, 1e-3)
+#
+# ocp.add_inequality_constraint(
+#     'path', M,
+#     lower_limit=mach_min, upper_limit=mach_max,
+#     regularizer=giuseppe.problems.automatic_differentiation.regularization.ADiffPenaltyConstraintHandler(
+#         regulator=eps_mach / mach_max
+#     )
+# )
+
 # Compilation
 with giuseppe.utils.Timer(prefix='Compilation Time:'):
     adiff_dual = giuseppe.problems.automatic_differentiation.ADiffDual(ocp)
-    num_solver = giuseppe.numeric_solvers.SciPySolver(adiff_dual, verbose=False, max_nodes=100, node_buffer=10,
+    num_solver = giuseppe.numeric_solvers.SciPySolver(adiff_dual, verbose=2, max_nodes=100, node_buffer=10,
                                                       bc_tol=1e-8)
 
-guess = giuseppe.guess_generation.auto_propagate_guess(adiff_dual, control=9 * np.pi/180, t_span=2.5)
+guess = giuseppe.guess_generation.auto_propagate_guess(adiff_dual, control=9 * np.pi/180, t_span=0.1)
 
 with open('guess_climb.data', 'wb') as file:
     pickle.dump(guess, file)

examples/ocp/maneuvers_supersonic/plot_optimal_climb.py
@@ -20,6 +20,21 @@ else:
         sols = pickle.load(f)
         sol = sols[-1]
 
+# Create Dicts
+k_dict = {}
+x_dict = {}
+lam_dict = {}
+u_dict = {}
+
+for key, val in zip(sol.annotations.constants, sol.k):
+    k_dict[key] = val
+
+for key, x_val, lam_val in zip(sol.annotations.states, list(sol.x), list(sol.lam)):
+    x_dict[key] = x_val
+    lam_dict[key] = lam_val
+
+for key, val in zip(sol.annotations.controls, list(sol.u)):
+    u_dict[key] = val
 
 # Constants
 r2d = 180 / np.pi
@@ -63,7 +78,7 @@ fig_u.tight_layout()
 
 # PLOT COSTATES
 ylabs = (r'$\lambda_{h}$', r'$\lambda_{V}$', r'$\lambda_{\gamma}$', r'$\lambda_{m}$')
-ymult = np.array((1., 1., 1., 1.))
+ymult = np.array((k_dict['h_ref'], k_dict['v_ref'], k_dict['gam_ref'], k_dict['m_ref']))
 fig_costates = plt.figure()
 axes_costates = []
 

giuseppe/numeric_solvers/bvp/scipy/solver.py
@@ -1,4 +1,5 @@
 from typing import Union, Optional
+from warnings import warn
 
 import numpy as np
 from scipy.integrate import solve_bvp
@@ -84,7 +85,8 @@ class SciPySolver:
                     tau_guess, x_guess, p_guess,
                     tol=self.tol, bc_tol=self.bc_tol, max_nodes=max_nodes, verbose=self.verbose
             )
-        except RuntimeError:
+        except RuntimeError as error:
+            warn(error.args[0])
             sol: _scipy_bvp_sol = self._form_solution_when_solver_throws_exception(tau_guess, x_guess, p_guess)
 
         return self.prob.post_process(sol, constants)