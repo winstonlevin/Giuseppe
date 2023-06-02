6f1bd7223d45391c80a901435ed617e2766d5908
winstonlevin <wlevin@purdue.edu>
2023-06-02 06:41:26 -0400

    Get set of gains for V/gam feedback

--- examples/ocp/maneuvers_supersonic/neighboring_feedback_gains.py
+++ examples/ocp/maneuvers_supersonic/neighboring_feedback_gains.py
@@ -30,15 +30,19 @@ for key, val in zip(sol.annotations.controls, list(sol.u)):
 
 # States
 h = ca.MX.sym('h')
+xn = ca.MX.sym('xn')
 v = ca.MX.sym('v')
 gam = ca.MX.sym('gam')
-x = ca.vcat((h, v, gam))
+
+x = ca.vcat((h, xn, v, gam))
+x_feedback = ca.vcat((v, gam))
 
 # Costates
 lam_h = ca.MX.sym('lam_h')
+lam_xn = ca.MX.sym('lam_xn')
 lam_v = ca.MX.sym('lam_v')
 lam_gam = ca.MX.sym('lam_gam')
-lam = ca.vcat((lam_h, lam_v, lam_gam))
+lam = ca.vcat((lam_h, lam_xn, lam_v, lam_gam))
 
 # Control
 alpha = ca.MX.sym('alpha', 1)
@@ -50,7 +54,6 @@ eta = k_dict['eta']
 mu = k_dict['mu']
 Re = k_dict['Re']
 m = x_dict['m'][0]
-lam_x = lam_dict['xn'][0] * k_dict['x_ref']
 
 # Atmosphere and Aerodynamics
 a = sped_table(h)
@@ -66,25 +69,23 @@ drag = qdyn * s_ref * (cd0 + eta * cla * alpha ** 2)
 
 # Equations of Motion
 dh_dt = v * ca.sin(gam)
+dxn_dt = v * ca.cos(gam)
 dv_dt = drag / m - g * ca.sin(gam)
 dgam_dt = lift / (m * v) - g / v * ca.cos(gam)
 
-f = ca.vcat((dh_dt, dv_dt, dgam_dt))
+f = ca.vcat((dh_dt, dxn_dt, dv_dt, dgam_dt))
+f_feedback = ca.vcat((dv_dt, dgam_dt))
 
 # Linearization of EoM
-A = ca.jacobian(f, x)
-B = ca.jacobian(f, u)
-
-# Path Cost (instead of x dynamics)
-dx_dt = v * ca.cos(gam)
-L = dx_dt * lam_x
+A = ca.jacobian(f_feedback, x_feedback)
+B = ca.jacobian(f_feedback, u)
 
 # Hamiltonian and Partial Derivatives
-ham = L + lam.T @ f
+ham = lam.T @ f
 
-Hx = ca.jacobian(ham, x)
+Hx = ca.jacobian(ham, x_feedback)
 Hu = ca.jacobian(ham, u)
-Hxx = ca.jacobian(Hx, x)
+Hxx = ca.jacobian(Hx, x_feedback)
 Hxu = ca.jacobian(Hx, u)
 Huu = ca.jacobian(Hu, u)
 
@@ -101,25 +102,30 @@ N_fun = ca.Function('Q', (x, lam, u), (N,), ('x', 'lam', 'u'), ('N',))
 
 # Generate Interpolators for K based on E ------------------------------------------------------------------------------
 e_vals = np.empty(sol.t.shape)
-k_h_vals = np.empty(sol.t.shape)
 k_v_vals = np.empty(sol.t.shape)
 k_gam_vals = np.empty(sol.t.shape)
+v_glide_vals = np.empty(sol.t.shape)
+gam_glide_vals = np.empty(sol.t.shape)
 
 for idx in range(len(sol.t)):
-    h_idx = x_dict['h'][idx]
-    v_idx = x_dict['v'][idx]
-    gam_idx = x_dict['gam'][idx]
-    x_idx = np.vstack((h_idx, v_idx, gam_idx))
+    # Start at last idx (minimum energy) for monotonically increasing sequence.
+    idx_rev = -1 - idx
+    h_idx = x_dict['h'][idx_rev]
+    xn_idx = x_dict['xn'][idx_rev]
+    v_idx = x_dict['v'][idx_rev]
+    gam_idx = x_dict['gam'][idx_rev]
+    x_idx = np.vstack((h_idx, xn_idx, v_idx, gam_idx))
 
     g_idx = mu / (Re + h_idx) ** 2
     e_idx = g_idx * h_idx + 0.5 * v_idx ** 2
 
-    lam_h_idx = lam_dict['h'][idx] * k_dict['h_ref']
-    lam_v_idx = lam_dict['v'][idx] * k_dict['v_ref']
-    lam_gam_idx = lam_dict['gam'][idx] * k_dict['gam_ref']
-    lam_idx = np.vstack((lam_h_idx, lam_v_idx, lam_gam_idx))
+    lam_h_idx = lam_dict['h'][idx_rev] * k_dict['h_ref']
+    lam_xn_idx = lam_dict['xn'][idx_rev] * k_dict['x_ref']
+    lam_v_idx = lam_dict['v'][idx_rev] * k_dict['v_ref']
+    lam_gam_idx = lam_dict['gam'][idx_rev] * k_dict['gam_ref']
+    lam_idx = np.vstack((lam_h_idx, lam_xn_idx, lam_v_idx, lam_gam_idx))
 
-    u_idx = np.vstack((u_dict['alpha'][idx],))
+    u_idx = np.vstack((u_dict['alpha'][idx_rev],))
 
     A_idx = np.asarray(A_fun(x_idx, lam_idx, u_idx))
     B_idx = np.asarray(B_fun(x_idx, lam_idx, u_idx))
@@ -131,10 +137,22 @@ for idx in range(len(sol.t)):
     K_idx = sp.linalg.solve(R_idx, B_idx.T @ P_idx + N_idx.T)
 
     e_vals[idx] = e_idx
-    k_h_vals[idx] = K_idx[0, 0]
-    k_v_vals[idx] = K_idx[0, 1]
-    k_gam_vals[idx] = K_idx[0, 2]
+    v_glide_vals[idx] = v_idx
+    gam_glide_vals[idx] = gam_idx
+    k_v_vals[idx] = K_idx[0, 0]
+    k_gam_vals[idx] = K_idx[0, 1]
 
-k_h_interp = sp.interpolate.pchip(e_vals, k_h_vals)
+v_glide_interp = sp.interpolate.pchip(e_vals, v_glide_vals)
+gam_glide_interp = sp.interpolate.pchip(e_vals, gam_glide_vals)
 k_v_interp = sp.interpolate.pchip(e_vals, k_v_vals)
 k_gam_interp = sp.interpolate.pchip(e_vals, k_gam_vals)
+
+interp_dict = {
+    'v': v_glide_interp,
+    'gam': gam_glide_interp,
+    'k_v': k_v_interp,
+    'k_gam': k_gam_interp
+}
+
+with open('neighboring_feedback_dict_range.data', 'wb') as file:
+    pickle.dump(interp_dict, file)
