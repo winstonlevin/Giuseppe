21487b2d1efa8511f84aef6f8520eb0ea57a62d8
winstonlevin <wlevin@purdue.edu>
2023-11-06 09:12:42 -0500

    Get optimal MTC with off-energy state initial conditions

--- examples/ocp/maneuvers/airplane2_flat_earth/airplane2_min_time_to_climb.py
+++ examples/ocp/maneuvers/airplane2_flat_earth/airplane2_min_time_to_climb.py
@@ -8,6 +8,7 @@ import giuseppe
 
 from airplane2_aero_atm import g, mass as mass0, s_ref, CD0_fun, CD1, CD2_fun, atm, lut_data,\
     load_max, dens_fun, sped_fun, max_ld_fun_mach, gam_qdyn0, CL0, CLa_fun, alpha_max, qdyn_max, thrust_fun, Isp
+from ardema_mae import find_climb_path
 
 SWEEP_SOLUTION_SPACE = True
 
@@ -32,7 +33,7 @@ v = v_nd * v_scale
 mass = m_nd * m_scale
 
 h_scale_val = 1e4
-v_scale_val = 1e4
+v_scale_val = 1e3
 m_scale_val = mass0
 ocp.add_constant(h_scale, h_scale_val)
 ocp.add_constant(m_scale, m_scale_val)
@@ -100,7 +101,7 @@ ocp.add_constraint('initial', mass - m_0)
 # Terminal state
 ocp.add_constraint('terminal', h - h_f)
 ocp.add_constraint('terminal', v - v_f)
-ocp.add_constraint('terminal', gam - gam_f)
+# ocp.add_constraint('terminal', gam - gam_f)
 
 with giuseppe.utils.Timer(prefix='Compilation Time:'):
     adiff_dual = giuseppe.problems.automatic_differentiation.ADiffDual(ocp)
@@ -136,71 +137,66 @@ if __name__ == '__main__':
         _x_guess = 0.5 * (_x_min + _x_max)
         return _x_guess
 
-    # Find velocity where h = 0
-    h_f = 0.
-    rho_f = atm.density(h_f)
-    a_f = atm.speed_of_sound(h_f)
-    v_f = binary_search(
-        10., 1e3,
-        lambda _v: rho_f * _v**2 * s_ref * max_ld_fun_mach(_v/a_f)['CL'] / (2*weight0) - 1, 0.
-    )
-    mach_f = v_f / atm.speed_of_sound(h_f)
+    # Initial Conditions
+    mach0 = 0.5
+    h0 = 40e3
+    v0 = mach0 * atm.speed_of_sound(h0)
+    gam0 = 0.
+    energy0 = g * h0 + 0.5 * v0**2
+    _climb_dict0 = find_climb_path(mass0, energy0, h_guess=8e3)
 
-    # Find altitude where Mach = 2.5
-    mach_0 = 2.5
-    CL_0 = float(max_ld_fun_mach(mach_0)['CL'])
-    h_0 = binary_search(
-        1e3, 90e3,
-        lambda _h: atm.density(_h) * (mach_0 * atm.speed_of_sound(_h))**2 * s_ref * CL_0 / (2*weight0) - 1, 0.
-    )
+    # Terminal Conditions
+    machf = 2.0
+    hf = 80e3
+    vf = machf * atm.speed_of_sound(hf)
+    energyf = g * hf + 0.5 * vf**2
+    _climb_dictf = find_climb_path(mass0, energyf, h_guess=40e3)
 
     def ctrl_law(_t, _x, _p, _k):
         _h = _x[0] * h_scale_val
-        _xd = _x[1] * xd_scale_val
-        _v = _x[2] * v_scale_val
-        _gam = _x[3]
+        _v = _x[1] * v_scale_val
+        _gam = _x[2]
+        _m = _x[3]
 
         _qdyn_s_ref = 0.5 * atm.density(_h) * _v**2 * s_ref
-        _CL_gam0 = weight0 / _qdyn_s_ref
+        _CL_gam0 = g*_m / _qdyn_s_ref
         return np.array((_CL_gam0,))
 
 
     # Flat earth costates
-    h_guess = h_0
-    v_guess = mach_0 * atm.speed_of_sound(h_guess)
-    gam_guess = gam_qdyn0(h_guess, v_guess)
-    CD_guess = float(CD0_fun(mach_0)) + CD1 * CL_0 + float(CD2_fun(mach_0)) * CL_0**2
-    CDu_guess = CD1 + 2 * float(CD2_fun(mach_0)) * CL_0
-    qdyn_s_ref_guess = 0.5 * atm.density(h_guess) * v_guess**2 * s_ref
-    drag_guess = qdyn_s_ref_guess * CD_guess
-    lam_e_glide = -mass / drag_guess
-    lam_h_glide = 0.
-    lam_gam_glide = lam_e_glide * v_guess**2 * CDu_guess
+    h_guess = _climb_dict0['h']
+    v_guess = _climb_dict0['V']
+    gam_guess = _climb_dict0['gam']
+    lam_e_ec = _climb_dict0['lam_E']
+    lam_h_ec = _climb_dict0['lam_h']
+    lam_gam_ec = _climb_dict0['lam_gam']
+    lam_m_ec = Isp / _climb_dict0['T']
 
     # Convert E,h,gam costates to h,V,gam costates
     # d(E)/dt = g d(h)/dt + V d(V)/dt
     # -> lam_v = V lam_E
     # -> lam_h = lam_h + g lam_E
-    lam_v_guess = lam_e_glide * v_guess
-    lam_h_guess = lam_h_glide + lam_e_glide / g
-    lam_gam_guess = lam_gam_glide
-    lam_xd_guess = 0.
+    lam_v_guess = lam_e_ec * v_guess
+    lam_h_guess = lam_h_ec + lam_e_ec / g
+    lam_gam_guess = lam_gam_ec
+    lam_m_guess = lam_m_ec
 
-    costate_scales = np.array((h_scale_val/xd_scale_val, 1., v_scale_val/xd_scale_val, 1./xd_scale_val))
-    lam_guess = np.array((lam_h_guess, lam_xd_guess, lam_v_guess, lam_gam_guess)) * costate_scales
+    costate_scales = np.array((h_scale_val, v_scale_val, 1., m_scale_val))
+    lam_guess = np.array((lam_h_guess, lam_v_guess, lam_gam_guess, lam_m_guess)) * costate_scales
 
     guess = giuseppe.guess_generation.auto_propagate_guess(
-        adiff_dual, control=ctrl_law, t_span=np.linspace(0., 25., 5),
-        initial_states=np.array((h_guess / h_scale_val, 0., v_guess / v_scale_val, gam_guess)), fit_states=False,
-        immutable_constants=('h_scale', 'v_scale'), initial_costates=lam_guess, fit_adjoints=False
+        adiff_dual, control=ctrl_law, t_span=np.linspace(0., 5., 5),
+        initial_states=np.array((h_guess / h_scale_val, v_guess / v_scale_val, gam_guess, mass0/m_scale_val)),
+        fit_states=False, immutable_constants=('h_scale', 'v_scale', 'm_scale'),
+        initial_costates=lam_guess, fit_adjoints=False
     )
 
-    with open('guess_range.data', 'wb') as file:
+    with open('guess_mtc.data', 'wb') as file:
         pickle.dump(guess, file)
 
     seed_sol = num_solver.solve(guess)
 
-    with open('seed_sol_range.data', 'wb') as file:
+    with open('seed_sol_mtc.data', 'wb') as file:
         pickle.dump(seed_sol, file)
 
     idx_hf = adiff_dual.annotations.constants.index('h_f')
@@ -210,107 +206,44 @@ if __name__ == '__main__':
     idx_v0 = adiff_dual.annotations.constants.index('v_0')
     idx_gam0 = adiff_dual.annotations.constants.index('gam_0')
 
-    # Create altitude/Mach interpolant
+    # Create altitude/velocity interpolant
     hf_seed = seed_sol.x[0, -1] * h_scale_val
-    vf_seed = seed_sol.x[2, -1] * v_scale_val
-    machf_seed = vf_seed / atm.speed_of_sound(hf_seed)
-    n_coninuations = 100
-    mach_vals = np.linspace(mach_f, machf_seed, n_coninuations)
-    h_vals = np.empty(mach_vals.shape)
-    for idx, mach_val in enumerate(mach_vals):
-        h_vals[idx] = binary_search(
-            h_f, h_0,
-            lambda _h: atm.density(_h) * (mach_val * atm.speed_of_sound(_h)) ** 2 * s_ref * CL_0 / (2 * weight0) - 1, 0.
-        )
-    h_interp = sp.interpolate.PchipInterpolator(mach_vals, h_vals)
+    vf_seed = seed_sol.x[1, -1] * v_scale_val
+    ef_seed = g * hf_seed + 0.5 * vf_seed**2
+    n_coninuations = 25
+    e_vals = np.linspace(ef_seed, energyf, n_coninuations)
+    h_vals = np.empty(e_vals.shape)
+    v_vals = np.empty(h_vals.shape)
+    h_previous = hf_seed
+    for idx, e_val in enumerate(e_vals):
+        _out_dict = find_climb_path(mass0, e_val, h_guess=h_previous)
+        h_previous = _out_dict['h']
+        h_vals[idx] = _out_dict['h']
+        v_vals[idx] = _out_dict['V']
+    h_interp = sp.interpolate.PchipInterpolator(v_vals, h_vals)
 
-    def glide_slope_continuation_xf(previous_sol, frac_complete):
-        _mach_f = machf_seed + frac_complete * (mach_f - machf_seed)
-        _hf = h_interp(_mach_f)
-        _vf = _mach_f * atm.speed_of_sound(_hf)
-        _gamf = gam_qdyn0(_hf, _vf)
 
-        previous_sol.k[idx_hf] = _hf
-        previous_sol.k[idx_vf] = _vf
-        previous_sol.k[idx_gamf] = _gamf
-        return previous_sol.k
+    def energy_state_continuation(previous_sol, frac_complete):
+        _constants = previous_sol.k.copy()
+        _vf = vf_seed + frac_complete * (vf - vf_seed)
+        _hf = h_interp(_vf)
+        _constants[idx_hf] = _hf
+        _constants[idx_vf] = _vf
+        return _constants
 
     cont = giuseppe.continuation.ContinuationHandler(num_solver, seed_sol)
-    cont.add_custom_series(n_coninuations, glide_slope_continuation_xf, 'Glide Slope (tf)')
+    cont.add_linear_series(1, {'h_f': h_interp(vf_seed)})
+    cont.add_custom_series(n_coninuations, energy_state_continuation, 'Energy State (hf, vf)')
+    cont.add_linear_series(15, {'h_0': h0, 'v_0': v0})
     sol_set = cont.run_continuation()
-    sol_set.save('sol_set_range.data')
+    sol_set.save('sol_set_mtc.data')
 
-    hf = sol_set.solutions[-1].x[0, -1] * h_scale_val
-    xdf = sol_set.solutions[-1].x[1, -1] * xd_scale_val
-    vf = sol_set.solutions[-1].x[2, -1] * v_scale_val
-    gamf = sol_set.solutions[-1].x[3, -1]
-    ef = g * hf + 0.5 * vf ** 2
-    h0 = sol_set.solutions[-1].x[0, 0] * h_scale_val
-    v0 = sol_set.solutions[-1].x[2, 0] * v_scale_val
-    gam0 = sol_set.solutions[-1].x[3, 0]
-    e0 = g * h0 + 0.5 * v0 ** 2
-
-    # Sweep Solution Space (perturb initial h/v with gam0 = 0, e0 const.)
-    if SWEEP_SOLUTION_SPACE:
-        def generate_energy_sweep_continuation(_h0_0, _h0_f):
-            def energy_sweep_continuation(previous_sol, frac_complete):
-                _constants = previous_sol.k.copy()
-
-                _h0 = previous_sol.k[idx_h0]
-                _v0 = previous_sol.k[idx_v0]
-                _e0 = g * _h0 + 0.5 * _v0**2
-
-                _h0_next = _h0_0 + frac_complete * (_h0_f - _h0_0)
-                _v0_next = (2 * (_e0 - g * _h0_next)) ** 0.5
-                _gam0_next = gam_qdyn0(_h0_next, _v0_next)
-                _constants[idx_h0] = _h0_next
-                _constants[idx_v0] = _v0_next
-                _constants[idx_gam0] = _gam0_next
-                return _constants
-            return energy_sweep_continuation
-
-        # Maximum altitude where stall occurs
-        h0_max = binary_search(
-            145e3, h0,
-            lambda _h:
-            atm.density(_h) * (e0 - g*_h) * s_ref
-            * float(CL0 + CLa_fun((2*(e0 - g*_h)**0.5/atm.speed_of_sound(_h))) * alpha_max)
-            / weight0 - 1, 0.
-        )
-
-        # Minimum altitude where initial dynamic pressure is maximized
-        h0_min = binary_search(
-            100., h0,
-            lambda _h: atm.density(_h) * (e0 - g*_h) / qdyn_max - 1, 0.
-        )
-
-        # Get Low altitude solution
-        cont = giuseppe.continuation.ContinuationHandler(num_solver, deepcopy(sol_set.solutions[-1]))
-        cont.add_custom_series(
-            100, generate_energy_sweep_continuation(h0, h0_min), 'Const. energy', keep_bisections=False
-        )
-        sol_set_sweep1 = cont.run_continuation()
-
-        cont = giuseppe.continuation.ContinuationHandler(num_solver, deepcopy(sol_set.solutions[-1]))
-        cont.add_custom_series(
-            100, generate_energy_sweep_continuation(h0, h0_max), 'Const. energy', keep_bisections=False
-        )
-        sol_set_sweep2 = cont.run_continuation()
-
-        sol_set_sweep = deepcopy(sol_set_sweep2)
-        sol_set_sweep.solutions = deepcopy([
-            sol_set_sweep1.solutions[-1], sol_set_sweep1.solutions[0], sol_set_sweep2.solutions[-1]
-        ])
-
-        sol_set_sweep.save('sol_set_range_sweep.data')
-
-        h0_case1 = sol_set_sweep.solutions[0].x[0, 0] * h_scale_val
-        xdf_case1 = sol_set_sweep.solutions[0].x[1, -1] * xd_scale_val
-        v0_case1 = sol_set_sweep.solutions[0].x[2, 0] * v_scale_val
-        gam0_case1 = sol_set_sweep.solutions[0].x[3, 0]
-        mach0_case1 = v0_case1 / atm.speed_of_sound(h0_case1)
-
-        h0_case3 = sol_set_sweep.solutions[-1].x[0, 0] * h_scale_val
-        xdf_case3 = sol_set_sweep.solutions[-1].x[1, -1] * xd_scale_val
-        v0_case3 = sol_set_sweep.solutions[-1].x[2, 0] * v_scale_val
-        gam0_case3 = sol_set_sweep.solutions[-1].x[3, 0]
+    # hf = sol_set.solutions[-1].x[0, -1] * h_scale_val
+    # mf = sol_set.solutions[-1].x[3, -1] * m_scale_val
+    # vf = sol_set.solutions[-1].x[1, -1] * v_scale_val
+    # gamf = sol_set.solutions[-1].x[2, -1]
+    # ef = g * hf + 0.5 * vf ** 2
+    # h0 = sol_set.solutions[-1].x[0, 0] * h_scale_val
+    # v0 = sol_set.solutions[-1].x[1, 0] * v_scale_val
+    # gam0 = sol_set.solutions[-1].x[2, 0]
+    # e0 = g * h0 + 0.5 * v0 ** 2
--- examples/ocp/maneuvers/airplane2_flat_earth/ardema_mae.py
+++ examples/ocp/maneuvers/airplane2_flat_earth/ardema_mae.py
@@ -4,6 +4,8 @@ import casadi as ca
 
 from airplane2_aero_atm import g, weight0, dens_fun, sped_fun, s_ref, CD0_fun, CD2_fun, thrust_fun
 
+_f_zero_converged_flag = 1
+
 # This example is Mark Ardema's solution to the Minimum Time to Climb problem by matched asymptotic expansions.
 # https://doi.org/10.2514/3.7161
 
@@ -67,16 +69,44 @@ zero_fun_es = ca.Function('Fz', (m, E, h), (zero_es,), ('m', 'E', 'h'), ('Fz',))
 grad_fun_es = ca.Function('DFz', (m, E, h), (grad_es,), ('m', 'E', 'h'), ('DFz',))
 
 
-def find_climb_path(mass, energy, h_guess):
-    _h = sp.optimize.fsolve(
+def solve_zero(mass, energy, h_guess):
+    _x_val, _, _flag, __ = sp.optimize.fsolve(
         func=lambda _x: np.asarray(zero_fun_es(mass, energy, _x[0])).flatten(),
         x0=np.array((h_guess,)),
-        fprime=lambda _x: np.asarray(grad_fun_es(mass, energy, _x[0]))
-    )[0]
+        fprime=lambda _x: np.asarray(grad_fun_es(mass, energy, _x[0])), full_output=True
+    )
+
+    return _x_val[0], _flag
+
+
+def find_climb_path(mass, energy, h_guess):
+    _h, _flag = solve_zero(mass, energy, h_guess)
+
+    if _flag != _f_zero_converged_flag:
+        h_search_vals = np.linspace(0., 2*h_guess, 1000)
+        fzero_search_vals = np.empty(h_search_vals.shape)
+        for jdx, h_search_val in enumerate(h_search_vals):
+            fzero_search_vals[jdx] = zero_fun_es(mass, energy, h_search_val)
+        sign_flips = np.not_equal(np.sign(fzero_search_vals[1:]), np.sign(fzero_search_vals[:-1]))
+
+        if np.any(sign_flips):
+            idx_sign_flips = np.where(sign_flips)[0]
+            sol_list = []
+            for idx_sign_flip in idx_sign_flips:
+                _h_guess_new = 0.5 * (h_search_vals[idx_sign_flip] + h_search_vals[idx_sign_flip + 1])
+                _h_new, _flag_new = solve_zero(mass, energy, _h_guess_new)
+                if _flag_new == _f_zero_converged_flag:
+                    sol_list.append(_h_new)
+
+            if len(sol_list) > 0:
+                sol_array = np.array(sol_list)
+                sol_distance = (sol_array - h_guess) ** 2
+                _h = sol_array[np.where(sol_distance == np.min(sol_distance))]
+
     _v = (2 * (energy - g * _h))**0.5
     _mach = _v / float(sped_fun(_h))
     _qdyn_s_ref = 0.5 * float(dens_fun(_h)) * _v**2 * s_ref
-    _thrust = thrust_fun(_mach, _h)
+    _thrust = float(thrust_fun(_mach, _h))
     _CD0 = float(CD0_fun(_mach))
     _CD2 = float(CD2_fun(_mach))
     _lift = g * mass
@@ -87,67 +117,72 @@ def find_climb_path(mass, energy, h_guess):
     _lam_gam = _lam_E * _v**2 * _DL
     _lam_h = 0.
     _gam = 0.
+    _G = np.asarray(G_fun(
+        mass, energy, _h, _gam, _lam_E, _lam_h, _lam_gam
+    ))
     _climb_dict = {
         'm': mass, 'E': energy, 'h': _h, 'V': _v, 'gam': _gam,
         'lam_E': _lam_E, 'lam_h': _lam_h, 'lam_gam': _lam_gam,
-        'L': _lift, 'D': _drag, 'T': _thrust, 'M': _mach, 'qdyn_s_ref': _qdyn_s_ref
+        'L': _lift, 'D': _drag, 'T': _thrust, 'M': _mach, 'qdyn_s_ref': _qdyn_s_ref,
+        'G': _G
     }
     return _climb_dict
 
 
-# NUMERICAL SOLUTION [from ref] ----------------------------------------------------------------------------------------
-r2d = 180 / np.pi
+if __name__ == '__main__':
+    # NUMERICAL SOLUTION [from ref] ------------------------------------------------------------------------------------
+    from matplotlib import pyplot as plt
 
-mass0 = weight0 / g
-h0 = 40e3
-mach0 = 0.5
-v0 = mach0 * float(sped_fun(h0))
-E0 = g * h0 + 0.5 * v0**2
+    r2d = 180 / np.pi
 
-# Outer Solution
-_outer_dict = find_climb_path(mass0, E0, 8.e3)
+    mass0 = weight0 / g
+    h0 = 40e3
+    mach0 = 0.5
+    v0 = mach0 * float(sped_fun(h0))
+    E0 = g * h0 + 0.5 * v0**2
 
-# Linearization about E
-G00 = np.asarray(G_fun(
-    mass0, E0, _outer_dict['h'], _outer_dict['gam'], _outer_dict['lam_E'], _outer_dict['lam_h'], _outer_dict['lam_gam']
-))
-eig_G00, eig_vec_G00 = np.linalg.eig(G00)
-idx_stable = np.where(np.real(eig_G00) < 0)
-idx_unstable = np.where(np.real(eig_G00) > 0)
-eig_vec_stable = eig_vec_G00[:, idx_stable[0]]
-eig_vec_unstable = eig_vec_G00[:, idx_unstable[0]]
-Vs = np.hstack((np.real(eig_vec_stable[:, 0].reshape(-1, 1)), np.imag(eig_vec_stable[:, 0].reshape(-1, 1))))
-Vu = np.hstack((np.real(eig_vec_unstable[:, 0].reshape(-1, 1)), np.imag(eig_vec_unstable[:, 0].reshape(-1, 1))))
-Vs_known = Vs[(0, 3), :]  # h0 fixed, gam0 free -> lam_gam0 = 0
-Vs_unknown = Vs[(1, 2), :]
+    # Outer Solution
+    _outer_dict = find_climb_path(mass0, E0, 8.e3)
 
-# Solve for unknown initial conditions
-dh0 = h0 - _outer_dict['h']
-dlam_gam0 = 0.
-z0_known = np.vstack((dh0, dlam_gam0))
-z0_unknown = Vs_unknown @ np.linalg.solve(Vs_known, z0_known)
-dgam0 = z0_unknown[0, 0]
-dlam_h0 = z0_unknown[1, 0]
+    # Linearization about E
+    G00 = np.asarray(G_fun(
+        mass0, E0, _outer_dict['h'], _outer_dict['gam'], _outer_dict['lam_E'], _outer_dict['lam_h'], _outer_dict['lam_gam']
+    ))
+    eig_G00, eig_vec_G00 = np.linalg.eig(G00)
+    idx_stable = np.where(np.real(eig_G00) < 0)
+    idx_unstable = np.where(np.real(eig_G00) > 0)
+    eig_vec_stable = eig_vec_G00[:, idx_stable[0]]
+    eig_vec_unstable = eig_vec_G00[:, idx_unstable[0]]
+    Vs = np.hstack((np.real(eig_vec_stable[:, 0].reshape(-1, 1)), np.imag(eig_vec_stable[:, 0].reshape(-1, 1))))
+    Vu = np.hstack((np.real(eig_vec_unstable[:, 0].reshape(-1, 1)), np.imag(eig_vec_unstable[:, 0].reshape(-1, 1))))
+    Vs_known = Vs[(0, 3), :]  # h0 fixed, gam0 free -> lam_gam0 = 0
+    Vs_unknown = Vs[(1, 2), :]
 
-z0 = np.array((dh0, dgam0, dlam_h0, dgam0))
-sol_ivp = sp.integrate.solve_ivp(fun=lambda t, z: (G00 @ z).flatten(), t_span=np.array((0., 60.)), y0=z0)
+    # Solve for unknown initial conditions
+    dh0 = h0 - _outer_dict['h']
+    dlam_gam0 = 0.
+    z0_known = np.vstack((dh0, dlam_gam0))
+    z0_unknown = Vs_unknown @ np.linalg.solve(Vs_known, z0_known)
+    dgam0 = z0_unknown[0, 0]
+    dlam_h0 = z0_unknown[1, 0]
 
-from matplotlib import pyplot as plt
+    z0 = np.array((dh0, dgam0, dlam_h0, dgam0))
+    sol_ivp = sp.integrate.solve_ivp(fun=lambda t, z: (G00 @ z).flatten(), t_span=np.array((0., 60.)), y0=z0)
 
-fig = plt.figure()
+    fig = plt.figure()
 
-ax1 = fig.add_subplot(211)
-ax1.plot(sol_ivp.t, sol_ivp.y[0, :] * g)
-ax1.grid()
-ax1.set_xlabel('t [s]')
-ax1.set_ylabel('dh [ft]')
+    ax1 = fig.add_subplot(211)
+    ax1.plot(sol_ivp.t, sol_ivp.y[0, :] * g)
+    ax1.grid()
+    ax1.set_xlabel('t [s]')
+    ax1.set_ylabel('dh [ft]')
 
-ax2 = fig.add_subplot(212)
-ax2.plot(sol_ivp.t, sol_ivp.y[1, :] * r2d)
-ax2.grid()
-ax2.set_xlabel('t [s]')
-ax2.set_ylabel('dgam [deg]')
+    ax2 = fig.add_subplot(212)
+    ax2.plot(sol_ivp.t, sol_ivp.y[1, :] * r2d)
+    ax2.grid()
+    ax2.set_xlabel('t [s]')
+    ax2.set_ylabel('dgam [deg]')
 
-fig.tight_layout()
+    fig.tight_layout()
 
-plt.show()
+    plt.show()
