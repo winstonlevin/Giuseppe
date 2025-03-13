0a3f9fd4511472345218246ca6bd2ab79693709c
winstonlevin <wlevin@purdue.edu>
2025-03-13 10:21:18 -0400

    Fix implementation bugs, try range of values

--- examples/ocp/energy_state_continuation/integration_comparison.py
+++ examples/ocp/energy_state_continuation/integration_comparison.py
@@ -18,6 +18,7 @@ def generate_pseudospectral_residual(dynamic_fun: ca.Function, n: int, col_metho
     y0 = ca.SX.sym('y0')
     ycol = ca.SX.sym('y', n)  # n collocation points
     tf = ca.SX.sym('tf')
+    y_vals = ca.vcat((y0, ycol))
 
     # Generate nondimensional collocation points
     _collocation_method = col_method.lower()
@@ -27,59 +28,55 @@ def generate_pseudospectral_residual(dynamic_fun: ca.Function, n: int, col_metho
         _, diff_mat = giuseppe.utils.pseudospectral.lagrange_matrices(
             col_points, col_points, compute_interp_matrix=False, compute_diff_matrix=True
         )
-        int_vec = col_weights @ diff_mat
+        yf = y0 + ca.dot(np.dot(col_weights, diff_mat), y_vals)
+        ycol_dot = diff_mat[1:, :] @ y_vals
         col_points = col_points[1:]
-        diff_mat = diff_mat[1:, :]  # Initial diff is for y0 -> no residual
-        y_vals = ca.vcat((y0, ycol))
     elif _collocation_method == 'lg':
         # Legendre-Gauss Quadrature
         (col_points, col_weights) = giuseppe.utils.pseudospectral.lg(n+1)
         _, diff_mat = giuseppe.utils.pseudospectral.lagrange_matrices(
             col_points, col_points[1:], compute_interp_matrix=False, compute_diff_matrix=True
         )  # Initial diff is for y0 -> no residual
+        yf = y0 + ca.dot(np.dot(col_weights, diff_mat), y_vals)
+        ycol_dot = diff_mat @ y_vals
         col_points = col_points[1:]
-        int_vec = col_weights @ diff_mat
-        y_vals = ca.vcat((y0, ycol))
     elif _collocation_method == 'lgr':
         # Legendre-Gauss-Radau Quadrature
         (col_points, col_weights) = giuseppe.utils.pseudospectral.lgr(n+1)
         _, diff_mat = giuseppe.utils.pseudospectral.lagrange_matrices(
             col_points, col_points, compute_interp_matrix=False, compute_diff_matrix=True
         )
-        int_vec = col_weights @ diff_mat
+        yf = y0 + ca.dot(np.dot(col_weights, diff_mat), y_vals)
+        ycol_dot = diff_mat[1:, :] @ y_vals
         col_points = col_points[1:]
-        diff_mat = diff_mat[1:, :]  # Initial diff is for y0 -> no residual
-        y_vals = ca.vcat((y0, ycol))
-    elif _collocation_method == 'lgrr':
+    elif _collocation_method == 'flgr':
         (col_points, col_weights) = giuseppe.utils.pseudospectral.lgr(n)
         col_points = -col_points[::-1]
         col_weights = col_weights[::-1]
         _, diff_mat = giuseppe.utils.pseudospectral.lagrange_matrices(
-            col_points, col_points, compute_interp_matrix=False, compute_diff_matrix=True
+            np.concatenate(((-1.,), col_points)), col_points, compute_interp_matrix=False, compute_diff_matrix=True
         )
-        int_vec = col_weights @ diff_mat
-        y_vals = ycol
+        diff0 = diff_mat[:, 0]
+        diff_mat = diff_mat[:, 1:]
+        ycol_dot = diff0*y0 + diff_mat @ ycol
+        yf = y0 + ca.dot(col_weights, ycol_dot)
     elif _collocation_method == 'cg':
         (col_points, col_weights) = giuseppe.utils.pseudospectral.cg(n+1)
         _, diff_mat = giuseppe.utils.pseudospectral.lagrange_matrices(
             col_points, col_points, compute_interp_matrix=False, compute_diff_matrix=True
         )
-        int_vec = col_weights @ diff_mat
+        yf = ca.dot(np.dot(col_weights, diff_mat), ca.vcat((y0, ycol)))
+        ycol_dot = diff_mat[1:, :] @ ca.vcat((y0, ycol))
         col_points = col_points[1:]
-        diff_mat = diff_mat[1:, :]  # Initial diff is for y0 -> no residual
-        y_vals = ca.vcat((y0, ycol))
     else:
         raise ValueError(
             f'col_method="{col_method}" is not implemented! Valid options are:\n'
-            f'\t["lgl", "lg", "lgr", "lgrr", "cg"]'
+            f'\t["lgl", "lg", "lgr", "flgr", "cg"]'
         )
 
-    # Residual function
-    res_sym = diff_mat @ y_vals - tf/2*dynamic_fun(ycol)
-    res_fun = ca.Function('rcol', (ycol, y0, tf), (res_sym,), ('yCol', 'y0', 'tf'), ('rcol',))
-
-    yf_sym = ca.dot(int_vec, y_vals)
-    yf_fun = ca.Function('yf', (ycol, y0, tf), (yf_sym,), ('yCol', 'y0', 'tf'), ('rcol',))
+    # Residual, final value, and collocation time functions
+    res_fun = ca.Function('rcol', (ycol, y0, tf), (ycol_dot - tf/2*dynamic_fun(ycol),), ('yCol', 'y0', 'tf'), ('rcol',))
+    yf_fun = ca.Function('yf', (ycol, y0, tf), (yf,), ('yCol', 'y0', 'tf'), ('rcol',))
     tcol_fun = ca.Function('tcol', (tf,), ((col_points + 1)*tf/2,), ('tf',), ('tcol',))
 
     return res_fun, yf_fun, tcol_fun
@@ -111,7 +108,7 @@ else:
     )
 
 # Integration scheme ------------------------------------------------------------------------------------------------- #
-num_col = 8
+cols_try = np.arange(2, 20+1, 1)
 collocation_method = 'lg'
 integration_scheme = 'pseudospectral'
 
@@ -126,25 +123,65 @@ else:
         f'["pseudospectral"]'
     )
 
-# True value
-res_1, yf_1, tcol_fun = generator(num_col)
-tcol_1 = tcol_fun(tf).full()[:, 0]
-ycol_1 = true_state_equation(tcol_1)
+sol_dicts: list[dict] = []
+for num_col in cols_try:
+    # True value
+    res_fun, yf_fun, tcol_fun = generator(num_col)
+    tcol = tcol_fun(tf).full()[:, 0]
+    ycol = true_state_equation(tcol).full()[:, 0]
 
-# Root
-ycol_sol = optimize.root(lambda _ycol: res_1(_ycol, y0, tf).full()[:, 0], ycol_1)
-ycol_1_hat = ycol_sol.x
+    # Root
+    ycol_sol = optimize.root(lambda _ycol: res_fun(_ycol, y0, tf).full()[:, 0], ycol)
+    ycol_hat = ycol_sol.x
+    yf_hat = yf_fun(ycol_hat, y0, tf).full()[0, 0]
+
+    # Errors
+    ecol = ycol_hat - ycol
+    norm_err = np.dot(ecol, ecol)**0.5
+
+    ef = yf_hat - yf
+
+    sol_dicts.append({
+        'n': num_col,
+        'tcol': np.append(tcol, tf),
+        'ycol': np.append(ycol, yf),
+        'ycol_hat': np.append(ycol_hat, yf_hat),
+        'ecol': np.append(ecol, ef),
+        'success': ycol_sol.success
+    })
 
 # -------------------------------------------------------------------------------------------------------------------- #
 # PLOTTING                                                                                                             #
 # -------------------------------------------------------------------------------------------------------------------- #
 t_vals = np.linspace(0, tf, 1_000)
 y_vals = true_state_equation(t_vals).full()[:, 0]
+y_min = y_vals.min(initial=np.inf)
+y_max = y_vals.max(initial=-np.inf)
+plot_buffer = 0.2
 
 # Plot state vs. time
 fig_y, ax_y = plt.subplots()
 ax_y.grid(zorder=-1)
 ax_y.plot(t_vals, y_vals, label='True')
-ax_y.scatter(tcol_1, ycol_1_hat, label='Pseudospectral')
+ycol_plot = ax_y.plot([], [], 'o', label='Pseudospectral')[0]
 ax_y.set_xlabel(r'$t$')
 ax_y.set_ylabel(r'$y(t)$')
+ax_y.set_xlim((-plot_buffer, tf + plot_buffer))
+
+
+def set_plot(_sol_dict):
+    ycol_plot.set_data(_sol_dict['tcol'], _sol_dict['ycol_hat'])
+    if _sol_dict["success"]:
+        convergence_str = "SUCCESS"
+    else:
+        convergence_str = "FAILURE"
+    ax_y.set_title(
+        f'PS Approximation [{_sol_dict["n"]} col. pts, {convergence_str}, ef={_sol_dict["ecol"][-1]:.2e}]'
+    )
+    ax_y.set_ylim((
+        min(_sol_dict['ycol_hat'].min(), y_min) - plot_buffer,
+        max(_sol_dict['ycol_hat'].max(), y_max) + plot_buffer
+    ))
+
+
+set_plot(sol_dicts[-1])
