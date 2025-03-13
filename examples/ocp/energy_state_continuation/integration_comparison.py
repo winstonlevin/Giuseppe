9c98f6fa0888f8b5c868f99a2703207eef917649
winstonlevin <wlevin@purdue.edu>
2025-03-13 14:50:17 -0400

    Add simple radius of convergence analysis

--- examples/ocp/energy_state_continuation/integration_comparison.py
+++ examples/ocp/energy_state_continuation/integration_comparison.py
@@ -82,6 +82,132 @@ def generate_pseudospectral_residual(dynamic_fun: ca.Function, n: int, col_metho
     return res_fun, yf_fun, tcol_fun
 
 
+def orthonormal_sampler(n: int, m: int, rng_seed=None):
+    """
+    Generate m sets of n orthogonal vectors of length n, randomly generated using the _rng_seed. Each set of n
+    orthogonal values is taken from the QR decomposition of the matrix H whose elements are normally distributed.
+    This algorithm is adapted from:
+    https://stackoverflow.com/questions/38426349/how-to-create-random-orthonormal-matrix-in-python-numpy
+
+    Parameters
+    ----------
+    n, int, length of each sample vector
+    m, int, number of sets of n orthogonal vectors to be generated
+    rng_seed, int or None, default=None, seed used in NumPy's i.i.d. standard normal generator
+
+    Returns
+    -------
+    list of samples
+    """
+    _generator = np.random.default_rng(seed=rng_seed)
+    _q_matrices = []
+    _samples = []
+
+    for _multiplicity_index in range(m):
+        _normal_matrix = _generator.random(size=(n, n))
+        _q, _r = np.linalg.qr(_normal_matrix, mode='complete')
+        _q = _q @ np.diag(np.sign(np.diag(_r)))
+        _samples.extend(_q)
+
+    return _samples
+
+
+def determine_radius_of_convergence(
+        root_finding_problem, sol_converged, multiplicity: int = 2, confidence: int = 0.99,
+        r_max: float = 100., return_continuous=False, rng_seed=None, max_iter: int=1_000, tol: float = 1E-3
+):
+    """
+    Determined the radius of convergence around the root z* via a binary search. The validity of a given radius is
+    determined by:
+        1. Generate co-varied points about the optimal solution at distance R away from optimal solution
+        2. Try to solve the root-finding problem with each guess. If the fraction of passed results are at least equal
+        to ``_confidence'', then the result is considered a pass.
+    NOTE: since a binary search is used, it is implicitly assumed that increasing R will lower the likelihood of
+    convergence.
+
+    Parameters
+    ----------
+    root_finding_problem, Callable, function taking in the initial guess and outputting a boolean flag of success.
+    sol_converged, (n,) np.ndarray, root of the root-finding problem about which to find the radius of convergence.
+    multiplicity, int, default=2, multiplicity*len(sol_converged) permutations are used to perturb samples.
+    confidence, float, default=0.99, fraction of values required to pass to be considered inside radius of convergence.
+                (ignored if return-continuous=True)
+    return_continuous, bool, default=False, whether to return a single r value or an interpolant of confidence vs. r
+    rng_seed, int or None, default=None, seed to be used in sampling perturbations to the guess vector
+    max_iter, int, default=1_000, maximum number of search steps to find radius of convergence
+    tol, float, default=1E-3, tolerance for binary search (break when upper/lower are within this value of each other)
+
+    Returns
+    -------
+    if _return_continuous, returns an interpolant of the fraction of successful results vs. radius of convergence.
+    Otherwise, returns radius of convergence as a float.
+    """
+    # Generate the unit vectors for the samples
+    sample_unit_vectors = orthonormal_sampler(n=len(sol_converged), m=multiplicity, rng_seed=rng_seed)
+
+    def _find_num_converged(_r):
+        return np.sum([root_finding_problem(sol_converged + _r * _sample) for _sample in sample_unit_vectors])
+
+    # Save list of prior values
+    r_values = []
+    n_pass_values = []
+
+    # Generate upper bound on radius of convergence
+    if return_continuous:
+        n_pass_min = 1
+    else:
+        n_pass_min = np.ceil(confidence * len(sample_unit_vectors)).astype(int)
+
+    r_upper = 1.
+    iterations_to_find_max = 0
+    upper_bound_active = False
+    for iterations_to_find_max in range(max_iter):
+        r_values.append(r_upper)
+        n_pass_values.append(_find_num_converged(r_upper))
+        # max_iter -= 1
+        if n_pass_values[-1] < n_pass_min:
+            # An upper bound on the failure rate is found
+            break
+        else:
+            # Continue increasing radius of convergence until breaking value is found
+            r_upper *= 2.
+
+            # Maximum bound on radius of convergence was discovered. If this max bound converges, return the maximum
+            if r_upper >= r_max:
+                r_upper = r_max
+                if upper_bound_active:
+                    # Ensure we only check the upper bound once
+                    break
+                upper_bound_active = True
+
+    if n_pass_values[-1] < n_pass_min:
+        # Conduct binary search to determine where n_pass_min occurs
+        r_lower = 0.
+        for iteration in range(max_iter - iterations_to_find_max):
+            r_values.append(0.5 * (r_lower + r_upper))
+            n_pass_values.append(_find_num_converged(r_values[-1]))
+
+            if n_pass_values[-1] < n_pass_min:
+                # This value is outside the radius of convergence
+                r_upper = r_values[-1]
+            else:
+                # This value is inside the radius of convergence
+                r_lower = r_values[-1]
+
+            if abs(r_lower - r_upper) < tol:
+                break
+    else:
+        r_lower = r_values[-1]
+
+    if return_continuous:
+        r_values = np.array(r_values)
+        pass_frac = np.array(n_pass_values) / len(sample_unit_vectors)
+        idces = np.argsort(r_values)
+        return interpolate.PchipInterpolator(r_values[idces], pass_frac[idces])
+    else:
+        # Return conservative bound
+        return r_lower
+
 # -------------------------------------------------------------------------------------------------------------------- #
 # NUMERICAL EXAMPLES                                                                                                   #
 # -------------------------------------------------------------------------------------------------------------------- #
@@ -108,7 +234,8 @@ else:
     )
 
 # Integration scheme ------------------------------------------------------------------------------------------------- #
-cols_try = np.arange(2, 20+1, 1)
+# cols_try = np.arange(2, 20+1, 1)
+cols_try = [5]
 collocation_method = 'lg'
 integration_scheme = 'pseudospectral'
 
@@ -148,14 +275,41 @@ for idx, num_col in enumerate(cols_try):
 
     ef = yf_hat - yf
 
+    # Determine radius of convergence numerically
+    if ycol_sol.success:
+        rconv = np.nan
+    else:
+        rconv = 0.
+
     sol_dicts.append({
         'n': num_col,
         'tcol': np.append(tcol, tf),
         'ycol': np.append(ycol, yf),
         'ycol_hat': np.append(ycol_hat, yf_hat),
         'ecol': np.append(ecol, ef),
-        'success': ycol_sol.success
+        'success': ycol_sol.success,
+        'rconv': rconv
     })
+
+# Experiment with radius of converged, TODO - remove
+tolerance_for_near = 1E-3 + np.dot(ycol_sol.fun, ycol_sol.fun)
+
+
+def _rfp(_z0):
+    _sol = optimize.root(lambda _ycol: res_fun(_ycol, y0, tf).full()[:, 0], _z0, jac=jac_fun, method='hybr')
+    _err = _sol.x - ycol_sol.x
+    return np.dot(_err, _err) < tolerance_for_near
+
+
+interp = determine_radius_of_convergence(_rfp, ycol_sol.x, return_continuous=True, r_max=1_000)
+
+fig_rcov, ax_rcov = plt.subplots()
+ax_rcov.grid(zorder=-1)
+r_vals = np.linspace(interp.x[0], interp.x[-1], 1000)
+ax_rcov.plot(r_vals, 100*interp(r_vals))
+ax_rcov.plot(interp.x, 100*interp(interp.x), 'o')
+ax_rcov.set_xlabel('r')
+fig_rcov.tight_layout()
 
 # -------------------------------------------------------------------------------------------------------------------- #
 # PLOTTING                                                                                                             #
