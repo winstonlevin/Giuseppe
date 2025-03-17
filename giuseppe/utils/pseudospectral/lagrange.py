6c24d511391b040478417275fa10ab5282c2901b
winstonlevin <wlevin@purdue.edu>
2025-03-17 12:10:15 -0400

    Remove commented out slow sympy implementation

--- giuseppe/utils/pseudospectral/lagrange.py
+++ giuseppe/utils/pseudospectral/lagrange.py
@@ -10,7 +10,6 @@ DOI: https://doi.org/10.21105/joss.02809.
 from typing import Optional
 
 import numpy as np
-# import sympy
 
 
 def lagrange_matrices(x_disc, x_interp, compute_interp_matrix=True, compute_diff_matrix=True):
@@ -87,9 +86,6 @@ def lagrange_matrices(x_disc, x_interp, compute_interp_matrix=True, compute_diff
 # -------------------------------------------------------------------------------------------------------------------- #
 # Lagrange Integration Matrix                                                                                          #
 # -------------------------------------------------------------------------------------------------------------------- #
-x_sym = sympy.Symbol('tau')
-
-
 def integration_matrix(x_disc: np.ndarray, x_interp: Optional[np.ndarray] = None):
     """
     A simple brute-force implementation of Lagrange integration matrices defined by x_disc, interpolated at x_interp
@@ -119,35 +115,3 @@ def integration_matrix(x_disc: np.ndarray, x_interp: Optional[np.ndarray] = None
     int_mat[np.abs(int_mat) < 1E-12] = 0.  # Set near-zero values to exactly zero
 
     return int_mat
-
-
-# def integration_matrix_sympy(x_disc: np.ndarray, x_interp: Optional[np.ndarray] = None):
-#     """
-#     A simple brute-force implementation of Lagrange integration matrices defined by x_disc, interpolated at x_interp
-#     """
-#     if x_interp is None:
-#         x_interp = x_disc
-#
-#     n_in = x_disc.shape[0]
-#     n_out = x_interp.shape[0]
-#
-#     # Generate Lagrange polynomials
-#     lagrange_polys = sympy.Matrix.zeros(n_in, 1)
-#     numerators = x_sym - x_disc
-#     denominators = x_disc[:, None] - x_disc[None, :]
-#     idces = np.arange(0, n_in, 1)
-#     for idx in idces:
-#         idces_remaining = np.delete(idces, idx)
-#         lagrange_polys[idx, 0] = sympy.prod(numerators[idces_remaining] / denominators[idx, idces_remaining])
-#
-#     # The integration matrix is defined as:
-#     # Aij = int(Lj, -1, taui)
-#     # [See Eq. 66 in https://doi.org/10.1016/j.automatica.2010.06.048]
-#     lagrange_poly_int = sympy.integrate(lagrange_polys, x_sym)
-#     lagrange_poly_int -= lagrange_poly_int.subs(x_sym, -1)  # Initial value is tau=-1
-#     int_mat = np.empty(shape=(n_out, n_in))
-#     for idx, xi in enumerate(x_interp):
-#         int_mat[idx:idx+1, :] = lagrange_poly_int.subs(x_sym, xi).T
-#     int_mat[np.abs(int_mat) < 1E-12] = 0.  # Set near-zero values to exactly zero
-#
-#     return int_mat
