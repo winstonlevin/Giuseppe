02e1aacf280992769b8976cc652e1a05559b1fcf
Winston C. Levin <winstonclevin@gmail.com>
2025-04-28 15:52:17 -0400

    Implement version finding a solution

--- examples/ocp/energy_state_continuation/planar_intercept/planar_intercept_nlp_reduced.py
+++ examples/ocp/energy_state_continuation/planar_intercept/planar_intercept_nlp_reduced.py
@@ -43,7 +43,7 @@ idces_fast = np.array((2,))  # The heading is the "fast" state
 # IF reduce_state -> Eliminate x(t), y(t), lamX(t), lamY(t),                        #
 #                    replace with integral constraint xf-x0-int(dx/dt dt) = 0       #
 # --------------------------------------------------------------------------------- #
-reduce_state = False
+reduce_state = True
 if reduce_state:
     idces_int_output = idces_slow  # Dynamics do not depend on (x,y)
     idces_state = idces_fast
@@ -76,43 +76,57 @@ else:
     nxr = nx
 
 n_phase = 1
-n_col = 20
+n_col = 8
 n_int = 30
 
-collocation_method = 'lg'
+collocation_method = 'lgl'
 
 if collocation_method == 'lg':
     col_points_local, col_weights_local = giuseppe.utils.pseudospectral.lg(n_col + 1)
     # col_weights_local = np.insert(col_weights_local, 0, 0)
     idces_anchor_local = np.arange(0, 1, 1)
     idces_collocation_local = np.arange(1, n_col + 1, 1)
+
+    proj_points_local, proj_weights_local = giuseppe.utils.pseudospectral.lg(n_int+1)
+    proj_points_local = proj_points_local[1:]
 elif collocation_method == 'lgr':
     col_points_local, col_weights_local = giuseppe.utils.pseudospectral.lgr(n_col)
     col_points_local = np.append(col_points_local, 1.)
     idces_anchor_local = np.array((n_col,))
     idces_collocation_local = np.arange(0, n_col, 1)
+
+    proj_points_local, proj_weights_local = giuseppe.utils.pseudospectral.lgr(n_int)
 elif collocation_method == 'lgl':
     col_points_local, col_weights_local = giuseppe.utils.pseudospectral.lgl(n_col)
     idces_anchor_local = np.empty(shape=(0,), dtype=int)
     idces_collocation_local = np.arange(0, n_col, 1)
+
+    proj_points_local, proj_weights_local = giuseppe.utils.pseudospectral.lgl(n_int)
 elif collocation_method == 'zlg':
     assert n_col % 2 == 0, f"ZLG requires an even number of collocation points, but n_col={n_col}!"
     col_points_local, col_weights_local = giuseppe.utils.pseudospectral.lg(n_col+1)
     col_points_local = np.sort(np.append(col_points_local[1:], 0))
     idces_anchor_local = np.where(col_points_local == 0)[0]
     idces_collocation_local = np.delete(np.arange(0, n_col+1, 1), idces_anchor_local)
+
+    proj_points_local, proj_weights_local = giuseppe.utils.pseudospectral.lg(n_int+1)
+    proj_points_local = proj_points_local[1:]
 elif collocation_method == 'zlgr':
     assert n_col % 2 == 0, f"ZLG requires an even number of collocation points, but n_col={n_col}!"
     col_points_local, col_weights_local = giuseppe.utils.pseudospectral.lgr(n_col)
     col_points_local = np.sort(np.append(col_points_local, 0))
     idces_anchor_local = np.where(col_points_local == 0)[0]
     idces_collocation_local = np.delete(np.arange(0, n_col+1, 1), idces_anchor_local)
+
+    proj_points_local, proj_weights_local = giuseppe.utils.pseudospectral.lgr(n_int)
 elif collocation_method == 'zlgl':
     assert n_col % 2 == 0, f"ZLGL requires an even number of collocation points, but n_col={n_col}!"
     col_points_local, col_weights_local = giuseppe.utils.pseudospectral.lgl(n_col)
     col_points_local = np.sort(np.append(col_points_local, 0))
     idces_anchor_local = np.where(col_points_local == 0)[0]
     idces_collocation_local = np.delete(np.arange(0, n_col+1, 1), idces_anchor_local)
+
+    proj_points_local, proj_weights_local = giuseppe.utils.pseudospectral.lgl(n_int)
 else:
     raise ValueError(f'collocation_method=={collocation_method} is not implemented!')
 
@@ -130,7 +144,19 @@ interp0f_mesh_local, _ = giuseppe.utils.pseudospectral.lagrange_matrices(
     col_points_local, np.array((-1, +1)), compute_interp_matrix=True, compute_diff_matrix=False
 )  # Interpolate Lam/U to get 0/f values
 
+projection_matrix_local, projection_diff_matrix_local = giuseppe.utils.pseudospectral.lagrange_matrices(
+    col_points_local, proj_points_local, compute_interp_matrix=True, compute_diff_matrix=True
+)
+projection_col_matrix_local, _ = giuseppe.utils.pseudospectral.lagrange_matrices(
+    col_points_local[idces_collocation_local], proj_points_local, compute_interp_matrix=True, compute_diff_matrix=False
+)
+
 # Expand values for multi-phase
+proj_mat = np.zeros(shape=(n_int*n_phase, n_mesh*n_phase))
+proj_col_mat = np.zeros(shape=(n_int*n_phase, n_col*n_phase))
+proj_diff_mat = np.zeros(shape=(n_int*n_phase, n_mesh*n_phase))
+proj_weights = np.zeros(shape=(n_int*n_phase,))
+
 diff_mat = np.zeros(shape=(n_col*n_phase, n_mesh*n_phase))
 int_mat = np.zeros(shape=(n_col*n_phase, n_col*n_phase))
 col_points = np.tile(col_points_local, n_phase)
@@ -142,6 +168,11 @@ interpf_col_matrix = np.zeros_like(interp0_col_matrix)
 interp0_mesh_matrix = np.zeros(shape=(n_mesh * n_phase, n_phase), dtype=interp0f_col_local.dtype)
 interpf_mesh_matrix = np.zeros_like(interp0_mesh_matrix)
 for phase in range(n_phase):
+    proj_mat[phase*n_int:(phase+1)*n_int, phase*n_mesh:(phase+1)*n_mesh] = projection_matrix_local
+    proj_col_mat[phase*n_int:(phase+1)*n_int, phase*n_col:(phase+1)*n_col] = projection_col_matrix_local
+    proj_diff_mat[phase*n_int:(phase+1)*n_int, phase*n_mesh:(phase+1)*n_mesh] = projection_diff_matrix_local
+    proj_weights[phase*n_int:(phase+1)*n_int] = proj_weights_local
+
     diff_mat[phase*n_col:(phase+1)*n_col, phase*n_mesh:(phase+1)*n_mesh] = diff_mat_local
     int_mat[phase*n_col:(phase+1)*n_col, phase*n_col:(phase+1)*n_col] = int_mat_local  # Diagonal block is current integration
     int_mat[phase*n_col:(phase+1)*n_col, :phase*n_col] = np.tile(col_weights_local[None, :], (n_col, phase,))
@@ -159,9 +190,11 @@ idces_anchor = np.concatenate([idces_anchor_local+n_mesh*_phase for _phase in ra
 
 X_sym = ca.SX.sym('X', nx, n_mesh*n_phase)  # Include initial state
 U_sym = ca.SX.sym('U', nu, n_col*n_phase)
-s_sym = ca.SX.sym('s')
 nx_mesh = nxr*n_mesh*n_phase
 nu_mesh = nu*n_col*n_phase
+
+Xproj_sym = X_sym @ proj_mat.T
+Uproj_sym = U_sym @ proj_col_mat.T
 
 idces_initial = np.arange(0, X_sym.shape[1], n_mesh)
 
@@ -174,22 +207,22 @@ z_sym = ca.vcat((
     tf_sym,
 ))
 
-L_col = path_cost_fun(X_sym[:, idces_collocation], U_sym)
-f_col = eom_fun(X_sym[:, idces_collocation], U_sym)
+L_proj = path_cost_fun(Xproj_sym, Uproj_sym)
+f_proj = eom_fun(Xproj_sym, Uproj_sym)
 X0i_sym = X_sym @ interp0_mesh_matrix
 Xfi_sym = X_sym @ interpf_mesh_matrix
 
-integrated_cost = end_cost_fun(Xfi_sym[:, -1]) + dt_phase/2 * (L_col @ col_weights)
-dynamic_constraint = (
-                             dt_phase/2*f_col[idces_state, :] - X_sym[idces_state, :] @ diff_mat.T
-                     ) * np.tile(col_weights[None, :], (nxr, 1))
+integrated_cost = end_cost_fun(Xfi_sym[:, -1]) + dt_phase/2 * (L_proj @ proj_weights)
+dynamic_residual = (
+    dt_phase/2*f_proj[idces_state, :] - X_sym[idces_state, :] @ proj_diff_mat.T
+) * np.tile(proj_weights[None, :], (nxr, 1))
 
-xf_int_sym = X_sym[idces_int_output, 0] + dt_phase/2*f_col[idces_int_output, :] @ col_weights
+xf_int_sym = X_sym[idces_int_output, 0] + dt_phase/2*f_proj[idces_int_output, :] @ proj_weights
 
-# if reduce_state:
-#     dynamic_constraint = ca.vcat((ca.vec(dynamic_constraint[2, :]), ca.sum2(dynamic_constraint[:2, :])))
-# else:
-dynamic_constraint = ca.vec(dynamic_constraint)
+# dynamic_constraint = ca.vec(dynamic_residual)
+collocated_residual = dynamic_residual @ proj_col_mat
+dynamic_constraint = ca.vec(collocated_residual)
+
 initial_state_constraint = X0i_sym[:, 0] - x0
 phase_linkage_constraint = X0i_sym[idces_state, 1:] - Xfi_sym[idces_state, :-1]
 terminal_state_constraint = Xfi_sym[idces_state, -1] - xf[idces_state]
